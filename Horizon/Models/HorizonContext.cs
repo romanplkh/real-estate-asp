a6cfec591f965dab4ab57366fb6f395a69ce61fb
Olena Stepanova <86sthelen@gmail.com>
2019-10-04 10:25:22 -0300

    created project folder

---
+++ Horizon/Models/HorizonContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Horizon.Models
+{
+    public class HorizonContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Authentication> Auths { get; set; }
+
+    }
+}
