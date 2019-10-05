7522913263bf8d4466e39eca7789cf43679d69ca
Roman Pelikh <pelikh.roman@gmail.com>
2019-10-05 14:53:36 -0300

    landing relocating

---
+++ Horizon/Controllers/HorizonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Horizon.Controllers
+{
+    public class HorizonController : Controller
+    {
+        // GET: Horizon
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
